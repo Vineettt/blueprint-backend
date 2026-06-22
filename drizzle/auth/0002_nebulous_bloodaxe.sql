95c317210269e60eddda4c0337253c00aee49c81
Vineettt <24775221+Vineettt@users.noreply.github.com>
2026-06-22 15:33:29 +0530

    chore: migration data

---
+++ drizzle/auth/0002_nebulous_bloodaxe.sql
@@ -0,0 +1,20 @@
+-- =====================================
+-- USERS
+-- =====================================
+
+INSERT INTO public.users (
+    id,
+    first_name,
+    last_name,
+    email,
+    password,
+    status
+)
+VALUES (
+    '019ecf8d-303d-7591-948e-4a8acc11ee05',
+    'string',
+    'string',
+    'vineeet.yadav@gmail.com',
+    '$argon2id$v=19$m=65536,t=3,p=1$cF1PZZQLfHOU96aE1V0bNw$Dkah1jbpeuBFc++NiUNNdHlG9BHVLL2dfVRJajjDTc8',
+    0
+);
